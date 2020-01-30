daba244f6583839c9d56196e56b1b59ef4cf830a
ezuri <32184698+ezuri@users.noreply.github.com>
2020-01-30 15:03:38 -0500

    Added basic file read capabilities

--- partialcat.py
+++ partialcat.py
@@ -1,15 +1,17 @@
 import argparse
 
-global rulefile, wordlist
+global rulefile		# Array containing the contents of the passed in rule file
+global wordlist		# Array containing the contents of the passed in wordlist
 
+# Parse command line options and read in the rule and word lists
 def parse_args():
 	global rulefile, wordlist
 	parser = argparse.ArgumentParser(description='Python utility to create special wordlist based on partial application of hashcat rules')
 	parser.add_argument('rulefile', type=argparse.FileType('r'), help='file name of a hashcat rule file')
 	parser.add_argument('wordlist', type=argparse.FileType('r'), help='file name of the wordlist')
 	args = parser.parse_args()
-	rulefile = args.rulefile
-	wordlist = args.wordlist
+	rulefile = args.rulefile.read().splitlines()
+	wordlist = args.wordlist.read().splitlines()
 
 def main():
 	parse_args()
