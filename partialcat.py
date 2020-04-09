f64dfc12566aa9218a0f5170cea08edbd75b7cad
ezuri <32184698+ezuri@users.noreply.github.com>
2020-04-09 16:29:07 -0400

    Removed global variables to clean up code

--- partialcat.py
+++ partialcat.py
@@ -1,18 +1,17 @@
 import argparse
 import string
 
-global rulefile		# Array containing the contents of the passed in rule file
-global wordlist		# Array containing the contents of the passed in wordlist
-
 # Parse command line options and read in the rule and word lists
+# Output: The rulefile and wordlist as arrays
 def parse_args():
-	global rulefile, wordlist
+	#global rulefile, wordlist
 	parser = argparse.ArgumentParser(description='Python utility to create special wordlist based on partial application of hashcat rules')
 	parser.add_argument('rulefile', type=argparse.FileType('r'), help='file name of a hashcat rule file')
 	parser.add_argument('wordlist', type=argparse.FileType('r'), help='file name of the wordlist')
 	args = parser.parse_args()
 	rulefile = args.rulefile.read().splitlines()
 	wordlist = args.wordlist.read().splitlines()
+	return rulefile, wordlist
 
 # Function called for rule 's'. Replaces instances of one character with another in a partial, iterative fashion
 # Input:
@@ -52,7 +51,7 @@ def parse(word, rule):
 
 def main():
 	# Parse input arguments and generate the wordlist
-	parse_args()
+	rulefile, wordlist = parse_args()
 	output = []
 	for word in wordlist:
 		for rule in rulefile:
